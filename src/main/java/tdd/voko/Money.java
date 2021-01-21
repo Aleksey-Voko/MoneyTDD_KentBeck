ea50749d22adac96836b639c38e9403093220f22
Aleksey Voko <voko.aleksey@gmail.com>
2021-01-21 14:02:16 +0400

    class Franc

--- src/main/java/tdd/voko/Money.java
+++ src/main/java/tdd/voko/Money.java
@@ -21,3 +21,25 @@ class Dollar {
         return amount == dollar.amount;
     }
 }
+
+
+class Franc {
+    private final int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        Franc franc = (Franc) o;
+        return amount == franc.amount;
+    }
+}
